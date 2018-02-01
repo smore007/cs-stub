b95c5b0a86b4078cf27befd723521cdf3716c10d
smore007 <30681316+smore007@users.noreply.github.com>
2018-01-31 21:29:11 -0700

    Added stub

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CsStub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = AppDomain.CurrentDomain.Load(bytes.shellcode); // takes a byte[] of a .net executable
+            MethodInfo Metinf = asm.EntryPoint;                          // to obtain the byte[] just use File.ReadAllBytes(exePath) or similar
+            object InjObj = asm.CreateInstance(Metinf.Name);
+            object[] parameters = new object[1]; // needed for most .net executables. they perform unexpectedly w/out a parameter object 
+            Metinf.Invoke(InjObj, parameters);
+        }
+    }
+}
